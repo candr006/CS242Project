4ad576f8c4d5597832ca7c690d5dcbe5714ea1a4
candr006 <candr006@ucr.edu>
2019-01-21 22:03:49 -0800

    part of the selector works

--- recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
+++ recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
@@ -7,16 +7,29 @@ class RecipeSpider(scrapy.Spider):
     ]
 
 	def parse(self, response):
-		for recipe in response.css('div.fd-recipe'):
-			next_page= recipe.css('div.fd-inner-tile div.fd-img-wrap div.inner-wrapper a::attr(href)').extract_first()
-			print("--------------------------------URL: "+next_page)
-			if next_page is not None:
-				print("---------------------------here")
-				next_page = response.urljoin(str(next_page))
-	        	yield scrapy.Request(str(next_page), callback=self.parse)
-	        	#response.url.split("/")[-2]
-		    	filename = 'outputrecipe-%s.html' % page
-		    	with open(filename, 'a') as f:
-		    		f.write(response.body)
+		body_tag=response.css("body div.fd-site-wrapper div.gk-tile-content")
+		i=1
+		for tc in body_tag:
+			if i==2:
+				print('-----------------------------body_tag '+str(body_tag))
+				recipe=tc.css("div.tile-stream div.fd-recipe")
+				print('-------------------recipe: '+str(recipe))
+				it= recipe.css('div.fd-inner-tile')
+				print("------------------it: "+str(it))
+				iw=it.css('div.fd-img-wrap')
+				print("------------------iw: "+str(iw))
+				next_page=iw.css('div.inner-wrapper a::attr(href)')
+
+				print("--------------------------------URL: ")
+				print(next_page)
+				if next_page is not None:
+					print("---------------------------here")
+					next_page = response.urljoin(str(next_page))
+		        	yield scrapy.Request(str(next_page), callback=self.parse)
+		        	#response.url.split("/")[-2]
+			    	filename = 'outputrecipe-%s.html'
+			    	with open(filename, 'a') as f:
+			    		f.write(response.body)
+			i+=1
 
 

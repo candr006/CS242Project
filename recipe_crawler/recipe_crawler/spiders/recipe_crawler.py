0f4ad8ea2defd93706083d996d7f4b4153d01f8b
candr006 <candr006@ucr.edu>
2019-01-30 23:26:11 -0800

    crawls genius kitchen

--- recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
+++ recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
@@ -8,17 +8,15 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12;rv:50.0) 
 cookie = cookielib.CookieJar()
 handler = urllib2.HTTPCookieProcessor(cookie)
 opener = urllib2.build_opener(handler)
-url="http://www.geniuskitchen.com/?pn=2"
+url="http://www.geniuskitchen.com/recipe"
 
 request = urllib2.Request(url = url, headers=headers)
-page = opener.open(request).read()
- 
-html_page = urllib2.urlopen(url)
+html_page = opener.open(request).read()
 soup = BeautifulSoup(html_page)
 links = []
 
 for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-	#if 'geniuskitchen' in link.get('href'):
+	#if "geniuskitchen" in link.get('href'):
     links.append(link.get('href'))
 
 recipe_facts=[]
@@ -26,14 +24,16 @@ ingredients=[]
 directions=[]
 
 for link in links:
-	html_page = urllib2.urlopen(link)
-	soup = BeautifulSoup(html_page)
+	response = requests.get(link, headers=headers)
+	soup = BeautifulSoup(response.text,"html.parser")
 	recipe_facts=soup.findAll("div", attrs={"class": "recipe-facts"})
 	ingredients=soup.findAll("div", attrs={"class": "ingredient-list"})
 	directions= soup.findAll("div", attrs={"class": "directions-inner container-xs"})
 
-
-	print("Recipe Facts:" +str(recipe_facts))
-	print("Ingredients: "+str(ingredients))
-	print("Directions: "+str(directions))
+	if recipe_facts:
+		print("Recipe Facts:" +str(recipe_facts))
+	if ingredients:
+		print("Ingredients: "+str(ingredients))
+	if directions:
+		print("Directions: "+str(directions))
 

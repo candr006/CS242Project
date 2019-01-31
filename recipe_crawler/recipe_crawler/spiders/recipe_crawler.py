e816e3f6ce61f95214e8fa64ff1067e1e568de87
candr006 <candr006@ucr.edu>
2019-01-30 21:32:34 -0800

    url request

--- recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
+++ recipe_crawler/recipe_crawler/spiders/recipe_crawler.py
@@ -1,30 +1,39 @@
 from bs4 import BeautifulSoup
 import urllib2
 import re
- 
-def getLinks(url):
-    html_page = urllib2.urlopen(url)
-    soup = BeautifulSoup(html_page)
-    links = []
- 
-    for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-    	#if 'geniuskitchen' in link.get('href'):
-        links.append(link.get('href'))
+import requests
+import cookielib
 
-    recipe_facts=[]
-    ingredients=[]
-    directions=[]
- 
-    for link in links:
-    	html_page = urllib2.urlopen(link)
-    	soup = BeautifulSoup(html_page)
-    	for rf in soup.findAll("div", {"class": "recipe-facts"}).getText():
-    		recipe_facts.append(rf)
-    	for i in soup.findAll("div", {"class": "ingredient-list"}).getText():
-    		ingredients.append(i)
-    	for d in soup.findAll("div", {"class": "directions-inner"}).getText():
-    		directions.append(d)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12;rv:50.0) Gecko/20100101 Firefox/50.0'}
+cookie = cookielib.CookieJar()
+handler = urllib2.HTTPCookieProcessor(cookie)
+opener = urllib2.build_opener(handler)
+url="http://www.geniuskitchen.com/?pn=2"
 
-    	print("Recipe Facts:" +recipe_facts)
-    	print("Ingredients: "+ingredients)
-    	print("Directions: "+directions)
+request = urllib2.Request(url = url, headers=headers)
+page = opener.open(request).read()
+ 
+html_page = urllib2.urlopen(url)
+soup = BeautifulSoup(html_page)
+links = []
+
+for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+	#if 'geniuskitchen' in link.get('href'):
+    links.append(link.get('href'))
+
+recipe_facts=[]
+ingredients=[]
+directions=[]
+
+for link in links:
+	html_page = urllib2.urlopen(link)
+	soup = BeautifulSoup(html_page)
+	recipe_facts=soup.findAll("div", attrs={"class": "recipe-facts"})
+	ingredients=soup.findAll("div", attrs={"class": "ingredient-list"})
+	directions= soup.findAll("div", attrs={"class": "directions-inner container-xs"})
+
+
+	print("Recipe Facts:" +str(recipe_facts))
+	print("Ingredients: "+str(ingredients))
+	print("Directions: "+str(directions))
+

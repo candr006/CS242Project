f7e67de7912948cf8281435bcb972cd3d617490c
candr006 <candr006@ucr.edu>
2019-02-01 19:37:10 -0800

    formatted output

--- recipe_crawler/recipe_crawler/spiders/foodiecrush.py
+++ recipe_crawler/recipe_crawler/spiders/foodiecrush.py
@@ -34,12 +34,38 @@ for link in links:
 		if(r.status_code==200):
 			html_page = requests.get(link['href'])
 			soup = BeautifulSoup(html_page.text, 'html.parser')
-			recipe_facts=soup.findAll("article", attrs={"class": "post"})
+			recipe_content=soup.findAll("p")
+			recipe_tags=soup.findAll("a", attrs={"class": "entry-tags"})
+			recipe_name=soup.findAll("h2",attrs={"class": "wprm-recipe-name"})
+			recipe_summary= soup.findAll("div",attrs={"class": "wprm-recipe-summary"})
+			ingredients= soup.findAll("li",attrs={"class": "wprm-recipe-ingredient"})
+			instructions= soup.findAll("div",attrs={"class": "wprm-recipe-instruction-text"})
 
-			if recipe_facts:
-				for fact in recipe_facts:
-					print(fact.get_text())
-				print("*********************************************************")
+			if recipe_name:
+				print("Recipe Content: \n")
+				for p in recipe_content:
+					print(p.get_text())
+
+				print("Recipe Tags: \n")
+				for i in recipe_tags:
+					print(i.get_text())
+
+				print("Recipe Name: \n")
+				for i in recipe_name:
+					print(i.get_text())
+
+				print("Recipe Summary: \n")
+				for i in recipe_summary:
+					print(i.get_text())
+
+				print("Recipe Ingredients: \n")
+				for i in ingredients:
+					print(i.get_text())
+
+				print("Recipe instructions: \n")
+				for i in instructions:
+					print(i.get_text())
+			print("*********************************************************")
 
 	#links2= getLinks(link['href'])
 	#links=links+links2

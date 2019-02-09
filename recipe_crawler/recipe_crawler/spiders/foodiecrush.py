7419169b78dc3ad122e865c6c790bf9a7e713e44
candr006 <candr006@ucr.edu>
2019-02-09 15:29:51 -0800

    no longer crawling duplicates

--- recipe_crawler/recipe_crawler/spiders/foodiecrush.py
+++ recipe_crawler/recipe_crawler/spiders/foodiecrush.py
@@ -18,52 +18,36 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12;rv:50.0) 
 cookie = cookielib.CookieJar()
 handler = urllib2.HTTPCookieProcessor(cookie)
 opener = urllib2.build_opener(handler)
+global_links = []
 
-f = open("foodie_crush_output.txt", "w")
-#--------------------------------------------------------------
-#This code from scrapehero.com prevents being blocked from crawling
+f = open("outputs/foodie_crush_output1.txt", "w")
 
-def get_proxies():
-    url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:10]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
-    return proxies
-
-proxies = get_proxies()
-proxy_pool = cycle(proxies)
-
-#End code from scrapehero.com
-#-------------------------------------------------------------
 def getLinks(url):
 	links=[]
 	if 'www.foodiecrush' in url:
 	    page = requests.get(url)
 	    soup = BeautifulSoup(page.text, 'html.parser')
-	    links = soup.find_all('a')
-	    for link in links:
-	    	if link not in links:
-	        	links.append(link)
+	    a_tags = soup.find_all('a')
+	    for a in a_tags:
+	    	if a['href'] not in global_links:
+	    		#print("Global Links: "+str(global_links))
+	    		#print("\n")
+	        	links.append(a['href'])
+	        	global_links.append(a['href'])
 	return links
 pg_count=1
+out_num=1
 for j in range(1,31):
 	print("-------------Menu page "+str(j)+" of 31-----------------")
-	links = getLinks("https://www.foodiecrush.com/category/recipes/page/"+str(j)+"/")
+	page_links = getLinks("https://www.foodiecrush.com/category/recipes/page/"+str(j)+"/")
 
-	for link in links:
-		#f.write(link['href'])
-		if 'www.' in link['href']:
+	for link in page_links:
+		if 'www.' in link:
 			sleep(randint(1,5))
-			request = urllib2.Request(url = link['href'], headers=headers)
+			request = urllib2.Request(url = link, headers=headers)
 			try:
-				for k in range(1,11):
-					proxy = next(proxy_pool)
-					r=requests.get(link['href'])
-					r.raise_for_status()
+				r=requests.get(link)
+				r.raise_for_status()
 			except requests.exceptions.HTTPError as errh:
 				print ("Http Error:",errh)
 			except requests.exceptions.ConnectionError as errc:
@@ -74,16 +58,16 @@ for j in range(1,31):
 				print ("OOps: Something Else",err)
 
 			if(r.status_code==200):
-				html_page = requests.get(link['href'])
+				html_page = requests.get(link)
 				f.write(html_page.text.encode('utf-8'))
-				print("Recipe page count: "+str(pg_count))
+				print("Recipe num: "+str(pg_count)+" - "+link)
 				pg_count+=1
-				#soup = BeautifulSoup(html_page.text, 'html.parser')
+				if(pg_count%40==0):
+					f.close()
+					out_num+=1
+					f = open("outputs/foodie_crush_output"+str(out_num)+".txt", "w")
 				
 
 				f.write("*********************************************************")
-
-		#links2= getLinks(link['href'])
-		#links=links+links2
 
 

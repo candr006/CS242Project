4e8a706725e5a7e29fc9d8cf6820ca75d604d7f9
candr006 <candr006@ucr.edu>
2019-02-10 20:42:57 -0800

    prevent duplicates and output url in results

--- CS242_Index/src/main/java/project/CS242_Index.java
+++ CS242_Index/src/main/java/project/CS242_Index.java
@@ -8,6 +8,7 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -21,6 +22,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 import static java.nio.file.Files.newBufferedReader;
 
@@ -31,7 +34,11 @@ import static java.nio.file.Files.newBufferedReader;
 public class CS242_Index 
 {
     public static String remove_html(String html) {
-        return Jsoup.parse(html).text();
+        if(html != null && !html.isEmpty()) {
+            String text = Jsoup.parse(html).text();
+            return text;
+        }
+        else return "";
     }
 
     public static void main( String[] args ) throws IOException, ParseException {
@@ -49,31 +56,43 @@ public class CS242_Index
         //iterate through all the crawled files
         String file_path="outputs/foodie_crush_output";
         Integer i=1;
-        while(i<2) {
-            Document doc = new Document();
-            try (BufferedReader r = newBufferedReader(Paths.get(file_path.concat(i.toString()).concat(".txt")),StandardCharsets.UTF_8)) {
-                String line;
-                String cleanLine;
-                while((line = r.readLine()) != null){
-                        cleanLine=remove_html(line);
-                        if(!cleanLine.isEmpty()) {
-                            //I put stars in the output to separate the recipes
-                            if(cleanLine.equals("*********************************************************")) {
+        while(i<32) {
+            if(i!=26) {
+                System.out.println("Page: " + i);
+                Document doc = new Document();
+                try (BufferedReader r = newBufferedReader(Paths.get(file_path.concat(i.toString()).concat(".txt")), StandardCharsets.UTF_8)) {
+                    String line;
+                    String cleanLine;
+                    String field_name;
+                    while ((line = r.readLine()) != null) {
+                        field_name="content";
+                        if(line.contains("<meta property=\"og:url\" content=\"")){
+                            String[] url_parts=line.split("<meta property=\"og:url\" content=\"");
+                            String url =url_parts[1].substring(0, url_parts[1].length() - 4);
+                            doc.add(new TextField("url", url, Field.Store.YES));
+                        }
+                        cleanLine = remove_html(line);
+
+                        if (!cleanLine.isEmpty()) {
+                            //I put stars in the crawler output to separate the recipes
+                            if (cleanLine.equals("*********************************************************")) {
                                 //when the line being read is stars (recipe separator), add the doc to the index and create a new doc
                                 indexWriter.addDocument(doc);
                                 doc = new Document();
 
-                            }else{
-                                doc.add(new TextField("content", cleanLine, Field.Store.YES));
+                            } else {
+                                doc.add(new TextField(field_name, cleanLine, Field.Store.YES));
                                 //print out what is being added to the document for testing purposes
-                                System.out.println("Reading Line: " + cleanLine);
+                                //System.out.println("Reading Line: " + cleanLine);
                             }
                         }
+                    }
+
                 }
-                i++;
 
-            }
 
+                }
+            i++;
         }
 
         indexWriter.close();
@@ -84,7 +103,7 @@ public class CS242_Index
         QueryParser parser = new QueryParser("content", analyzer);
 
         //This is the query that you'll be indexing the documents by
-        Query query = parser.parse("chicken");
+        Query query = parser.parse("turkey");
 
         System.out.println("");
         System.out.println("------------------------ RESULTS --------------------------");
@@ -95,8 +114,8 @@ public class CS242_Index
         // Iterate through the results:
         for (int rank = 0; rank < hits.length; ++rank) {
             Document hitDoc = indexSearcher.doc(hits[rank].doc);
-            System.out.println((rank + 1) + " (score:" + hits[rank].score + ")--> " + hitDoc.get("content"));
-//             System.out.println(indexSearcher.explain(query, hits[rank].doc));
+            System.out.println((rank + 1) + " (score:" + hits[rank].score + ")--> " + hitDoc.get("content")+" -- "+hitDoc.get("url"));
+
         }
         indexReader.close();
         directory.close();
